c9ca52e88831d5a93c4df55a5ac110786bdedf38
Hemant Bharadwaj <hebharad@microsoft.com>
2021-04-19 01:35:01 +0530

    Add project files.

---
+++ ApplicationSlowness/About.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="About.aspx.cs" Inherits="About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Slowness detected</h3>
+    <p>Page loaded after 
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+&nbsp;Miliseconds.</p>
+</asp:Content>
---
+++ ApplicationSlowness/About.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading;
+
+public partial class About : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Random random = new Random();
+        int x = random.Next(60000);
+        Thread.Sleep(x);
+        Label1.Text = x.ToString();
+    }
+}
